48a2822eb43694662c7e28d14028febb9abe17ed
shah <shah@LAPTOP-0I10HQJ1>
2022-05-09 10:01:38 -0400

    New commit

--- src/Unit_01/P1_Task01_FirstJavaProgramInJava.java
+++ src/Unit_01/P1_Task01_FirstJavaProgramInJava.java
@@ -1,10 +1,13 @@
 package Unit_01;
 
-public class hello {
+public class P1_Task01_FirstJavaProgramInJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+
+			System.out.println("Hello World!");
+
+			
 	}
 
 }
